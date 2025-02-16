8e9e0124a29fdc9b12ff5164cf9e64dc6b020ee0
Taylor Tam <taylor@taylortam.com>
2025-02-15 19:53:46 -0800

    some UI stuff

--- feethacks/views/WelcomeView.swift
+++ feethacks/views/WelcomeView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 
 struct WelcomeView: View {
     @Binding var appMode: AppMode
+    @Environment(\.colorScheme) var colorScheme
    
     var body: some View {
-        Button() {
-            appMode = AppMode.ar
-        } label: {
-            Text("Turn on AR Scanning")
+        VStack {
+            Spacer()
+            Text("Solemate")
+                .font(.system(size: 35, weight: .bold, design: .rounded))
+                .padding(.vertical)
+            Text("Find your perfect shoe match")
+                .padding(.vertical)
+            Spacer()
+            Button {
+                appMode = AppMode.ar
+            } label: {
+                Text("Create an AR Scan")
+                    .font(.headline)
+                    .foregroundColor(colorScheme == .dark ? .white : .black) // Ensure text is visible
+                    .padding()
+                    .frame(width: 200)
+                    .background(colorScheme == .dark ? Color.black : Color.white) // Add a background
+                    .cornerRadius(15)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(colorScheme == .dark ? Color.white : Color.black, lineWidth: 2) // Outline border
+                    )
+            }
+            Spacer()
         }
     }
 }
